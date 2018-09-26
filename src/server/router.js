df331ad03e1c93e18e2531a5711acf2a81f8877b
sakib3 <sakib3@gmail.com>
2018-09-26 10:21:50 +0200

    use moongose to connect db

--- src/server/router.js
+++ src/server/router.js
@@ -1,10 +1,8 @@
-//const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const config = require('./config');
 const userRoutes = require('./api/routes/userRoutes');
 
-
-//mongoose.Promise = global.Promise;
-//mongoose.connect(config.mongoURI, {useMongoClient: true});
+mongoose.connect(config.mongoURI, { promiseLibrary: global.Promise, useNewUrlParser: true });
 
 module.exports = {
     api: [userRoutes]
