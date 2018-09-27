298e79730d00e9210ace34e6591bfedbb1338028
sakib3 <sakib3@gmail.com>
2018-09-27 12:57:38 +0200

    map mongoose date to readable format for view

--- src/server/api/controllers/timeReportController.js
+++ src/server/api/controllers/timeReportController.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const model = mongoose.model('TimeReport');
+const moment = require('moment');
 
 exports.getTimeReportForAll =
   function (req, res) {
@@ -16,6 +17,16 @@ exports.getTimeReportForUser =
       end: { "$lt": req.body.endDate }
     };
     model.find(query, function (err, data) {
-      res.json(data);
+      let report = data.map(function(d){
+        return {
+          user_id: d.user_id,
+          user_name: d.user_name,
+          type_id: d.type_id,
+          hours: d.hours,
+          start: moment(d.start).format('YYYY-DD-MM'),
+          end: moment(d.end).format('YYYY-DD-MM')
+        }
+      });
+      res.json(report);
     });
   };
